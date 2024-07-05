a244e6358b49c14a891330a3d994d4f159c4d364
itsraakhi <96717114+itsraakhi@users.noreply.github.com>
2024-07-05 12:07:15 +0530

    Add seed files

--- api/utils/Email.js
+++ api/utils/Email.js
@@ -7,7 +7,7 @@ class Email {
 		this.to = user.email;
 		this.firstName = user.name.split(' ')[0];
 		this.url = url;
-		this.from = `Abhinav Aggarwal <${process.env.EMAIL_FROM}>`;
+		this.from = `Raakhi Prajapati <${process.env.EMAIL_FROM}>`;
 	}
 
 	_newTransport() {
